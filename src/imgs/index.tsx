88fc40e9b805835b07bb704c10a5e70c7c7736f1
estidlore <estidlore@outlook.com>
2022-09-29 11:07:21 -0500

    hmi design

--- src/imgs/index.tsx
+++ src/imgs/index.tsx
@@ -1,5 +1,6 @@
 import carsInside from "./carsInside.png";
 import ctu from "./ctu.png";
+import hmi from "./hmi.png";
 import sequenceDiagram from "./sequenceDiagram.jpeg";
 import systemOnRestriction from "./systemOnRestriction.png";
 import ticketRestriction from "./ticketRestriction.png";
@@ -8,6 +9,7 @@ import tof from "./tof.png";
 export {
   carsInside,
   ctu,
+  hmi,
   sequenceDiagram,
   systemOnRestriction,
   ticketRestriction,
--- src/views/Design.tsx
+++ src/views/Design.tsx
@@ -2,7 +2,7 @@ import type { FC } from "react";
 import React from "react";
 
 import { Section } from "components";
-import { sequenceDiagram } from "imgs";
+import { hmi, sequenceDiagram } from "imgs";
 
 
 const Design: FC = (): JSX.Element => (
@@ -12,6 +12,13 @@ const Design: FC = (): JSX.Element => (
       {"un vehículo por el sistema y se plamó en un diagrama de secuencias."}
     </p>
     <img className={"my2 wp100"} src={sequenceDiagram} />
+    <p className={"my2"}>
+      {"Para el diseño del HMI, el encendido y apagado del sistema se usa "}
+      {"mediante un botón, al igual que el retiro y pago del tiquete. Los "}
+      {"sensores se simulan mediante un par de botones y el conteo de autos "}
+      {"se muestra mediante cuadros con texto variable."}
+    </p>
+    <img className={"my2 wp100"} src={hmi} />
   </Section>
 );
 
